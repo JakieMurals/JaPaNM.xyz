cf16a19eaa575de47409462e455e8071ca1bb99d
Vicky Nyaga <vickynyaga@Kile.local>
2025-04-28 03:56:29 +0300

    Github pages update, setting the site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://vbartalis.github.io",
+  site: "https://github.com/JakieMurals",
+  base: "/JaPaNM.xyz",
 });
