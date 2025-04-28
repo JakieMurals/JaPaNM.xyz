b4902542ae194d1520f4e5b5cf71972bf7db63d8
Vicky Nyaga <vickynyaga@Kile.local>
2025-04-28 04:10:31 +0300

    Additional retry with correct file path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://github.com/JakieMurals/JaPaNM.xyz",
+  site: "https://jakiemurals.github.io",
+  base: "/JaPaNM.xyz/",
 });
